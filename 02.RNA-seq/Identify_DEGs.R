c13110922b8d7b794893a60bcacccd1cbe6cff4c
NiniWang <wangnini_112@163.com>
2024-06-13 15:32:26 +0200

    version 1

--- 02.RNA-seq/Identify_DEGs.R
+++ 02.RNA-seq/Identify_DEGs.R
@@ -1,35 +1,54 @@
-getwd()
+# Load necessary library
 library("DESeq2")
+
 # Generate a matrix of lengths of each gene in each sample to use for cross-ortholog normalization
-gene_effectiveL<-read.table("hypo_L_GeneEffectiveLength.txt",header = T,sep="\t",row.names = 1)
-head(gene_effectiveL)
+# Read the effective lengths of genes from a file
+gene_effectiveL <- read.table("merged_{species1}_{species2}_EffectiveLength.output", header = TRUE, sep = "\t", row.names = 1)
 
 # Create a table of reads count and their conditions
-raw_count<-read.table("hypo_L_Readscount.txt",header = T,sep="\t",row.names = 1)
-head(raw_count)
+# Read the raw count data from a file
+raw_count <- read.table("merged_{species1}_{species2}_NumReads.output", header = TRUE, sep = "\t", row.names = 1)
 
-colDate<-data.frame(row.names = c("hypo_mouse_L_1","hypo_mouse_L_2","hypo_mouse_L_3","hypo_NMR_L_1","hypo_NMR_L_2","hypo_NMR_L_3"),
-                    condition=factor(c("control","control","control","NMR","NMR","NMR")),
-                    levels=c("NMR","control"))
+# Define the conditions for each sample
+colDate <- data.frame(
+  row.names = c("norm_mouse_H_1", "norm_mouse_H_2", "norm_mouse_H_3", "norm_NMR_H_1", "norm_NMR_H_2", "hnorm_NMR_H_3"),
+  condition = factor(c("control", "control", "control", "NMR", "NMR", "NMR")),
+  levels = c("NMR", "control")
+)
 
-dds<-DESeqDataSetFromMatrix(countData = raw_count,colData = colDate,design = ~condition)
-dds = DESeq2::estimateSizeFactors(dds, normMatrix = gene_effectiveL)
+# Create DESeq2 dataset
+dds <- DESeqDataSetFromMatrix(countData = raw_count, colData = colDate, design = ~condition)
+
+# Normalize the counts using the effective lengths of genes
+dds <- DESeq2::estimateSizeFactors(dds, normMatrix = gene_effectiveL)
+
+# Filter out low count genes
 keep <- rowSums(counts(dds)) >= 10
-dds <- dds[keep,]
-######Differential expression analysis####
-dds <- DESeq(dds)
-res<-results(dds)
-class(res)
-res<-as.data.frame(res)
-res<-cbind(rownames(res),res)
-colnames(res)<- c('geng_id',"baseMean" ,"log2FoldChange","lfcSE" ,"stat","pvalue","padj" )
-write.table(res,"hypo_mouseVSnmr_L_all-DESeq2.gene.txt",sep = '\t',col.names = T,row.names = F,quote = FALSE,na='')
+dds <- dds[keep, ]
 
-resSig<-res[which(res$padj<0.01 & abs(res$log2FoldChange)>1.5),]
-##新增一列，将log2FoldChange>0标注为up，<0标准为down
-resSig[which(resSig$log2FoldChange>0),'up_down']<-'up'
-resSig[which(resSig$log2FoldChange<0),'up_down']<-'down'
-##保存数据
-write.table(resSig,"Norm_L-diff-0.05-FC_1.gene.xls",sep = '\t',
-            col.names = T,row.names = F,quote = FALSE,na='')
+# Differential expression analysis
+dds <- DESeq(dds)
+
+# Extract results and convert to a data frame
+res <- results(dds)
+class(res)
+res <- as.data.frame(res)
+
+# Add row names as a column and rename columns
+res <- cbind(rownames(res), res)
+colnames(res) <- c('gene_id', "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")
+
+# Save the results to a file
+write.table(res, "norm_mouseVSnmr_H_all-DESeq2.gene.txt", sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE, na = '')
+
+# Filter significant results based on adjusted p-value and log2 fold change
+resSig <- res[which(res$padj < 0.01 & abs(res$log2FoldChange) > 1.5), ]
+
+# Add a new column to indicate upregulated and downregulated genes
+resSig[which(resSig$log2FoldChange > 0), 'up_down'] <- 'up'
+resSig[which(resSig$log2FoldChange < 0), 'up_down'] <- 'down'
+
+# Save the significant results to a file
+write.table(resSig, "norm_mouseVSnmr_H_DEGs.xls", sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE, na = '')
+
 
--- 02.RNA-seq/RNA-seq_analysis.sh
+++ 02.RNA-seq/RNA-seq_analysis.sh
@@ -1,14 +1,18 @@
-#step1
-#build index
+#step1 build index
 salmon index -t {species}_longest_trans.fa -i {species}_longest_trans_salmonindex -p 20
 
-#step2
-#quantification. Use 
+#step2 quantification 
 sample=$1 #sample name
 salmon quant -i {file_pathway}/{salmonindex} -l A -g {file_pathway}/{gtf_file} -1 {file_pathway}/${sample}_1_val_1.fq.gz -2 {file_pathway}/${sample}_2_val_2.fq.gz  -p 8 -o ${sample}
 
-#step3
-#merge results from same species
+#step3 merge results from same species
 python merge_salmon_result.py -folders {sample1} {sample2} {sample3}  -col NumReads -out merged_{species}_NumReads.csv
 python merge_salmon_result.py -folders {sample1} {sample2} {sample3}  -col TPM -out merged_{species}_TPM.csv
 python merge_salmon_result.py -folders {sample1} {sample2} {sample3}  -col EffectiveLength -out merged_{species}_EffectiveLength.csv
+
+#step4 merge NumReads/EffectiveLength/TPM results from different species into same matrix based on the result in 03.Orthologous_Across_Species
+python MergeResultAcrossSpecies.py 
+merged_{species1}_{species2}_{NumReads/EffectiveLength/TPM}.output
+
+#step5 identify DEGs
+# see Identify_DEGs.R script
