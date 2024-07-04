6a384e34881d4ebe7350d62c16e4baf2b9565dfd
NiniWang <wangnini_112@163.com>
2024-07-04 23:17:44 +0200

    version 1

--- 04.plot/VolcanoPlot.R
+++ 04.plot/VolcanoPlot.R
@@ -9,7 +9,7 @@ ggplot(data = raw_count_filt, aes(x = log2FoldChange, y = -log10(padj), color = 
   geom_point(alpha = 0.8, size = 1) + xlim(-20, 20) +
   theme_bw(base_size = 15) +
   theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank()) +
-  geom_hline(yintercept = -log10(0.05), linetype = 4) +
-  geom_vline(xintercept = c(-1, 1), linetype = 4) +
+  geom_hline(yintercept = -log10(0.01), linetype = 4) +
+  geom_vline(xintercept = c(-1.5, 1.5), linetype = 4) +
   scale_color_manual(name = "", values = c("coral3", "seagreen4", "grey"), limits = c("UP", "DOWN", "NONE"))
 
